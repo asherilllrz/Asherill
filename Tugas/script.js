73e93952043304f3cd12b2b73093dab99e854e98
asherilllrz <asherilrizky@gmail.com>
2025-08-05 14:00:14 +0700

    Add files via upload

---
+++ Tugas/script.js
@@ -0,0 +1,30 @@
+
+window.onscroll = function () {
+  let btn = document.getElementById("scrollTopBtn");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+const teks = "Hi, Aku Asheril!";
+let i = 0;
+const speed = 100;
+
+function typeEffect() {
+  if (i < teks.length) {
+    document.getElementById("type-text").innerHTML += teks.charAt(i);
+    i++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+
+
+
+
+
